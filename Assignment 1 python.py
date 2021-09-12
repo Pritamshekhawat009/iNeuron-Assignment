419d831848d3fc37e969e3b92abe4ec7f3a4de24
Pritamshekhawat009 <90270680+Pritamshekhawat009@users.noreply.github.com>
2021-09-12 13:57:03 +0530

    Add files via upload

---
+++ Assignment 1 python.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Python Assignment 1
+# 
+# Quetion 1
+
+# In[20]:
+
+
+ist = [p for p in range (2000,3200) 
+        if p % 7 == 0 and p % 5 != 0]
+
+
+# In[21]:
+
+
+print (list)
+
+
+# In[ ]:
+
+
+
+
+
+# Quetion 2
+
+# In[24]:
+
+
+a=str(input ("Enter your frist Name  ") )
+b=str(input ("Enter your last Name  ") )
+c= a + b
+
+
+# In[25]:
+
+
+c
+
+
+# In[26]:
+
+
+c[::-1]
+
+
+# In[ ]:
+
+
+
+
+
+# Quetion 3 
+
+# In[17]:
+
+
+d= 12
+r= d/2
+
+
+# In[18]:
+
+
+r
+
+
+# In[19]:
+
+
+π = 3.14
+
+
+# In[20]:
+
+
+v = 4/3 * π *r
+
+
+# In[21]:
+
+
+v
+
+
+# In[27]:
+
+
+d= 12
+r= d/2
+π= 3.14
+v= 4/3*π*r
+
+
+# In[28]:
+
+
+v
+
+
+# In[ ]:
+
+
+
+
