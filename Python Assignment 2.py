8547497d4ae58d25494d0c96261ad2dff4795052
Pritamshekhawat009 <90270680+Pritamshekhawat009@users.noreply.github.com>
2021-09-23 21:53:37 +0530

    Add files via upload

---
+++ Python Assignment 2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assigment 2-First Quation 1
+
+# In[12]:
+
+
+n = 6
+
+for i in range(0,n):
+    print(""*(n-i),"."* i,""*(n-i))
+for i in range(n-2,0,-1):
+    print(""*(n-i),"."* i,""*(n-i))
+
+
+# # First Quation 2
+# 
+
+# In[19]:
+
+
+n= "ineuron"
+n[::-1]
+
+
+# In[ ]:
+
+
+
+
